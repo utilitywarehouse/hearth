a1d7ab870a709706cef6dfb6f7c3c7f4ff660e89
Rob Phoenix <rphoenix@uw.co.uk>
2024-11-13 10:59:12 +0000

    add storybook

--- packages/react/.eslintrc.cjs
+++ packages/react/.eslintrc.cjs
@@ -1,6 +1,6 @@
 /* eslint-env node */
 module.exports = {
-  extends: ['../../.eslintrc.js'],
+  extends: ['../../.eslintrc.js', 'plugin:storybook/recommended'],
   parserOptions: {
     project: './tsconfig.json',
     tsconfigRootDir: __dirname,
---
+++ packages/react/.storybook/preview.ts
@@ -0,0 +1,10 @@
+import { Preview } from '@storybook/react';
+import '@utilitywarehouse/fontsource';
+import '@utilitywarehouse/css-reset';
+import '../src/components/Box/Box.css';
+
+const preview: Preview = {
+  parameters: {},
+};
+
+export default preview;
