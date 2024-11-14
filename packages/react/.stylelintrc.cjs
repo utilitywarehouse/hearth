462fafd0b54e704e336b32640085f6c070653370
Rob Phoenix <rphoenix@uw.co.uk>
2024-11-14 13:20:10 +0000

    fix ci?

--- packages/react/.stylelintrc.cjs
+++ packages/react/.stylelintrc.cjs
@@ -3,6 +3,7 @@ module.exports = {
   plugins: ['stylelint-media-use-custom-media'],
   rules: {
     'csstools/media-use-custom-media': 'always',
+    'at-rule-no-unknown': [true, { ignoreAtRules: ['breakpoints'] }],
     // Enforce prefixes on classnames and keyframes
     'selector-class-pattern': /^((mobile|tablet|desktop|wide):)?-?uwp-([a-zA-Z]|-)+$/,
     'keyframes-name-pattern': /^uwp-([a-z]|-)+$/,
