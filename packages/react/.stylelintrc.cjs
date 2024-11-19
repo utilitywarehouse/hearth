830ac7965192c7a2c2eb8b531c74718da5525156
Rob Phoenix <rphoenix@uw.co.uk>
2024-11-19 11:51:18 +0000

    simplify css

--- packages/react/.stylelintrc.cjs
+++ packages/react/.stylelintrc.cjs
@@ -2,10 +2,11 @@ module.exports = {
   extends: 'stylelint-config-standard',
   plugins: ['stylelint-media-use-custom-media'],
   rules: {
-    'csstools/media-use-custom-media': 'always',
+    'csstools/media-use-custom-media': ['known', { importFrom: ['./src/styles/breakpoints.css'] }],
     'at-rule-no-unknown': [true, { ignoreAtRules: ['breakpoints'] }],
     // Enforce prefixes on classnames and keyframes
-    'selector-class-pattern': /^((mobile|tablet|desktop|wide):)?-?uwp-([a-zA-Z]|-)+$/,
+    'selector-class-pattern': /^((mobile|tablet|desktop|wide):)?-?uwp-([a-zA-Z\d]|-)+$/,
+    'custom-property-pattern': /([a-zA-Z\d]|-)+$/,
     'keyframes-name-pattern': /^uwp-([a-z]|-)+$/,
   },
 };
