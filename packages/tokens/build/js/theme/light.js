e396293eb6f6cba0357850ef8ab1f60676815acf
Jord McCord <jmccord@uw.co.uk>
2025-01-22 09:25:16 +0000

    chore: add tokens package and figma plugin (#25)

---
+++ packages/tokens/build/js/theme/light.js
@@ -0,0 +1,14 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export default {
+  light: {
+    components: {
+      button: {
+        "background-color": "#ffb921",
+        "border-color": "#101010",
+      },
+    },
+  },
+};
