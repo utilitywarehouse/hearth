6e81088a69e5240aaa7017ba63dafd1b3a16875b
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2025-01-24 11:57:12 +0000

    update space tokens (#51)

--- packages/react/src/props/gap.props.ts
+++ packages/react/src/props/gap.props.ts
@@ -8,6 +8,7 @@ const gapTokens = [
   '75',
   '100',
   '150',
+  '175',
   '200',
   '250',
   '300',
@@ -23,12 +24,18 @@ const gapTokens = [
 
 const gapPropDefs = {
   gap: { className: 'gap', tokens: gapTokens, responsive: true },
+  rowGap: { className: 'row-gap', tokens: gapTokens, responsive: true },
+  columnGap: { className: 'column-gap', tokens: gapTokens, responsive: true },
 } satisfies {
   gap: PropDef<(typeof gapTokens)[number]>;
+  rowGap: PropDef<(typeof gapTokens)[number]>;
+  columnGap: PropDef<(typeof gapTokens)[number]>;
 };
 
 interface GapProps {
   gap?: Responsive<Union<string, (typeof gapTokens)[number]>>;
+  rowGap?: Responsive<Union<string, (typeof gapTokens)[number]>>;
+  columnGap?: Responsive<Union<string, (typeof gapTokens)[number]>>;
 }
 
 export { gapPropDefs, gapTokens };
