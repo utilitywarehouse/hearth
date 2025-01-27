84cdd6acf3adef3ea2baa93798a01cdd21e4b5ee
Rob Phoenix <rphoenix@uw.co.uk>
2025-01-27 21:52:27 +0000

    add display prop to box

--- packages/react/src/components/Box/Box.props.ts
+++ packages/react/src/components/Box/Box.props.ts
@@ -2,8 +2,18 @@ import type { ColorProps } from '../../props/color.props';
 import type { GridItemProps } from '../../props/grid-item.props';
 import type { MarginProps } from '../../props/margin.props';
 import type { PaddingProps } from '../../props/padding.props';
+import { PropDef } from '../../props/prop-def';
 import type { SizeProps } from '../../props/size.props';
 import type { ComponentPropsWithout, RemovedProps } from '../../types/component-props';
+import { Responsive } from '../../types/responsive';
+
+const displayValues = ['none', 'inline', 'inline-block', 'block'] as const;
+
+export const boxPropDefs = {
+  display: { className: 'display', tokens: displayValues, responsive: true },
+} satisfies {
+  display: PropDef<(typeof displayValues)[number]>;
+};
 
 interface CommonBoxProps extends ColorProps, PaddingProps, MarginProps, SizeProps, GridItemProps {
   as?: 'div' | 'span';
@@ -11,6 +21,7 @@ interface CommonBoxProps extends ColorProps, PaddingProps, MarginProps, SizeProp
    * Change the default rendered element for the one passed as a child, merging their props and behavior.
    */
   asChild?: boolean;
+  display?: Responsive<(typeof displayValues)[number]>;
 }
 type BoxDivProps = { as?: 'div' } & ComponentPropsWithout<'div', RemovedProps>;
 type BoxSpanProps = { as: 'span' } & ComponentPropsWithout<'span', RemovedProps>;
