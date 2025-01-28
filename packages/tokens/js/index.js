a95e0f0039980ff364ee80950f32d73b470ca7fb
Jord McCord <jmccord@uw.co.uk>
2025-01-28 14:16:14 +0000

    feat(tokens): add js tokens generation (#59)

---
+++ packages/tokens/js/index.js
@@ -0,0 +1,9 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export { default as color } from './color.js';
+export { default as layout } from './layout.js';
+export { default as primitive } from './primitive.js';
+export { default as typography } from './typography.js';
+export * as components from './components/index.js';
---
+++ packages/tokens/js/layout.js
@@ -0,0 +1,76 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export const mobile = {
+  "content": {
+    "spacing-md": 16,
+    "spacing-lg": 16,
+    "spacing-xs": 8,
+    "spacing-xl": 28,
+    "padding-top": 24,
+    "spacing-sm": 12,
+    "padding-bottom": 32
+  },
+  "grid": {
+    "gutter": 16,
+    "grid-size": 8,
+    "column-number": 4,
+    "margin": 16,
+    "column-width": 64
+  },
+  "container": {
+    "padding-horizontal": 16,
+    "width": 360
+  }
+};
+
+export const tablet = {
+  "content": {
+    "spacing-md": 16,
+    "spacing-lg": 24,
+    "spacing-xs": 8,
+    "spacing-xl": 28,
+    "padding-top": 24,
+    "spacing-sm": 12,
+    "padding-bottom": 32
+  },
+  "grid": {
+    "gutter": 24,
+    "grid-size": 8,
+    "column-number": 8,
+    "margin": 32,
+    "column-width": 64
+  },
+  "container": {
+    "padding-horizontal": 32,
+    "width": 744
+  }
+};
+
+export const desktop = {
+  "content": {
+    "spacing-md": 16,
+    "spacing-lg": 24,
+    "spacing-xs": 8,
+    "spacing-xl": 40,
+    "padding-top": 32,
+    "spacing-sm": 12,
+    "padding-bottom": 48
+  },
+  "grid": {
+    "gutter": 24,
+    "grid-size": 8,
+    "column-number": 12,
+    "margin": 32,
+    "column-width": 64
+  },
+  "container": {
+    "padding-horizontal": 32,
+    "width": 1096
+  }
+};
+
+const layout = { mobile, tablet, desktop };
+
+export default layout;
