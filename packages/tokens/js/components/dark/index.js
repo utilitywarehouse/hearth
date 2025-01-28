c73611de4d4695c9aa98d133ec95fe6ff09a6ff9
Jord McCord <jmccord@uw.co.uk>
2025-01-28 14:09:41 +0000

    feat: add js tokens genetation

---
+++ packages/tokens/js/components/dark/index.js
@@ -0,0 +1,5 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export { default as button } from './button.js';
---
+++ packages/tokens/js/components/index.js
@@ -0,0 +1,6 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export * as light from './light/index.js';
+export * as dark from './dark/index.js';
