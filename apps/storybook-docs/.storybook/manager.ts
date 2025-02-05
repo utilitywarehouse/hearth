bdad8da69518b4f61dfef1ea63b9ca3cfdef8d8c
Jord McCord <jmccord@uw.co.uk>
2025-02-05 16:32:32 +0000

    fix: styles import

--- apps/storybook-docs/.storybook/manager.ts
+++ apps/storybook-docs/.storybook/manager.ts
@@ -1,4 +1,6 @@
 import { addons } from '@storybook/manager-api';
+import '@utilitywarehouse/hearth-css-reset';
+import '@utilitywarehouse/hearth-fonts';
 import theme from './theme';
 
 addons.setConfig({
--- packages/react-native/.storybook/manager.ts
+++ packages/react-native/.storybook/manager.ts
@@ -1,5 +1,6 @@
 import { addons } from '@storybook/manager-api';
 import theme from './theme';
+import '@utilitywarehouse/hearth-fonts';
 
 addons.setConfig({
   theme: theme,
