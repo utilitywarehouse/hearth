a413263b64a32828f40805bd8338eee8c3b6fe18
Jord McCord <jmccord@uw.co.uk>
2025-02-05 11:45:53 +0000

    feat: add `DetailText` component

--- packages/react-native/src/components/DetailText/DetailText.stories.tsx
+++ packages/react-native/src/components/DetailText/DetailText.stories.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Meta, StoryObj } from '@storybook/react';
-import { BodyText } from '.';
+import { DetailText } from '.';
 import { coloursAsArray } from '../../utils';
 import { VariantTitle } from '../../../docs/components';
 import { Box } from '../Box';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'React Native / Components / BodyText',
-  component: BodyText,
+  title: 'React Native / Components / DetailText',
+  component: DetailText,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
@@ -16,7 +16,7 @@ const meta = {
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
     size: {
-      options: ['sm', 'md', 'lg'],
+      options: ['sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl'],
       control: 'select',
       description: 'The size of the text.',
     },
@@ -25,11 +25,7 @@ const meta = {
       control: 'boolean',
       description: 'Truncate the text.',
     },
-    bold: {
-      type: 'boolean',
-      control: 'boolean',
-      description: 'Bold the text.',
-    },
+
     italic: {
       type: 'boolean',
       control: 'boolean',
@@ -88,17 +84,16 @@ const meta = {
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: {
-    children: "Hello there, I'm some body text!",
+    children: "Hello there, I'm some detail text!",
     size: 'md',
     truncated: false,
-    bold: false,
     italic: false,
     underline: false,
     strikeThrough: false,
     color: 'grey1000',
     textDecorationColor: 'grey1000',
   },
-} satisfies Meta<typeof BodyText>;
+} satisfies Meta<typeof DetailText>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -109,34 +104,47 @@ export const KitchenSink: Story = {
   render: args => (
     <Box gap="200">
       <VariantTitle title="SM">
-        <BodyText {...args} size="sm">
-          Hello there, I'm some body text!
-        </BodyText>
+        <DetailText {...args} size="sm">
+          Hello there, I'm some detail text!
+        </DetailText>
       </VariantTitle>
       <VariantTitle title="MD">
-        <BodyText {...args} size="md">
-          Hello there, I'm some body text!
-        </BodyText>
+        <DetailText {...args} size="md">
+          Hello there, I'm some detail text!
+        </DetailText>
       </VariantTitle>
       <VariantTitle title="LG">
-        <BodyText {...args} size="lg">
-          Hello there, I'm some body text!
-        </BodyText>
+        <DetailText {...args} size="lg">
+          Hello there, I'm some detail text!
+        </DetailText>
+      </VariantTitle>
+      <VariantTitle title="XL">
+        <DetailText {...args} size="xl">
+          Hello there, I'm some detail text!
+        </DetailText>
+      </VariantTitle>
+      <VariantTitle title="2XL">
+        <DetailText {...args} size="2xl">
+          Hello there, I'm some detail text!
+        </DetailText>
+      </VariantTitle>
+      <VariantTitle title="3XL">
+        <DetailText {...args} size="3xl">
+          Hello there, I'm some detail text!
+        </DetailText>
+      </VariantTitle>
+      <VariantTitle title="4XL">
+        <DetailText {...args} size="4xl">
+          Hello there, I'm some detail text!
+        </DetailText>
       </VariantTitle>
     </Box>
   ),
 };
 
-export const Bold: Story = {
-  args: {
-    bold: true,
-    children: "Hello there, I'm some bold body text!",
-  },
-};
-
 export const Underline: Story = {
   args: {
     underline: true,
-    children: "Hello there, I'm some underlined body text!",
+    children: "Hello there, I'm some underlined detail text!",
   },
 };
