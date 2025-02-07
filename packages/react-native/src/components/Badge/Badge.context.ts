93b6a6f273b00ffc8bfc15c399b61960deaacbc9
Jord McCord <jmccord@uw.co.uk>
2025-02-07 10:45:30 +0000

    feat(react-native): add `Badge` variants (#97)

--- packages/react-native/src/components/Badge/Badge.context.ts
+++ packages/react-native/src/components/Badge/Badge.context.ts
@@ -3,6 +3,7 @@ import type BadgeProps from './Badge.props';
 
 export const BadgeContext = React.createContext<{
   colorScheme?: BadgeProps['colorScheme'];
+  variant?: BadgeProps['variant'];
 }>({});
 
 export const useBadgeContext = () => useContext(BadgeContext);
--- packages/tokens/js/components/dark/badge.js
+++ packages/tokens/js/components/dark/badge.js
@@ -2,28 +2,49 @@
  * Do not edit directly, this file was auto-generated.
  */
 export default {
-    "blue": {
-        "backgroundColor": "#93c1ff"
-    },
     "borderRadius": 4,
-    "color": "#fffbe8",
     "flatBase": {
         "borderBottomLeftRadius": 0,
         "borderBottomRightRadius": 0
     },
     "gap": 4,
-    "green": {
-        "backgroundColor": "#7bd5aa"
-    },
-    "grey": {
-        "backgroundColor": "#a19e91"
-    },
-    "orange": {
-        "backgroundColor": "#ffcca8"
+    "outline": {
+        "blue": {
+            "color": "#93c1ff"
+        },
+        "borderColor": "#565352",
+        "borderWidth": 1,
+        "green": {
+            "color": "#7bd5aa"
+        },
+        "grey": {
+            "color": "#a19e91"
+        },
+        "orange": {
+            "color": "#ffcca8"
+        },
+        "red": {
+            "color": "#ffa89d"
+        }
     },
     "paddingHorizontal": 6,
     "paddingVertical": 2,
-    "red": {
-        "backgroundColor": "#ffa89d"
+    "solid": {
+        "color": "#fffbe8",
+        "blue": {
+            "backgroundColor": "#93c1ff"
+        },
+        "green": {
+            "backgroundColor": "#7bd5aa"
+        },
+        "grey": {
+            "backgroundColor": "#a19e91"
+        },
+        "orange": {
+            "backgroundColor": "#ffcca8"
+        },
+        "red": {
+            "backgroundColor": "#ffa89d"
+        }
     }
 };
