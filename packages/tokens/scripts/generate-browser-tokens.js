08a1e2000ac6a52b349338fe78c8af0a8ba4f74a
Rob Phoenix <rphoenix@uw.co.uk>
2025-03-13 16:18:47 +0000

    alternative implementation

--- packages/tokens/scripts/generate-browser-tokens.js
+++ packages/tokens/scripts/generate-browser-tokens.js
@@ -35,7 +35,7 @@ export * from './typography.js';
 
 StyleDictionary.registerFormat({
   name: 'browser/variables',
-  format: ({ dictionary, file }) => {
+  format: ({ dictionary, file, platform }) => {
     const tokens = {};
     // get the group name of the current set of tokens
     let tokensName = file.destination.split('.')[0];
@@ -45,7 +45,7 @@ StyleDictionary.registerFormat({
       // normalize the tokens in the same way they are for the CSS
       const normalizedPath = normalizeTokenPath(token);
       // this will give us the generated CSS variable
-      const cssCustomProperty = `var(--${PREFIX}-${kebabCase(normalizedPath.join(DELIMITER))})`;
+      const cssCustomProperty = `var(--${kebabCase([platform.prefix, ...normalizedPath].join(DELIMITER))})`;
       var tokenObject = {};
       // we need to build the JS object from the path, this will give us the
       // dot notation path to the token
@@ -96,6 +96,7 @@ export function generateBrowserTokens() {
       platforms: {
         browser: {
           buildPath: BUILD_PATH,
+          prefix: PREFIX,
           files: [
             {
               destination: 'color.ts',
@@ -136,10 +137,12 @@ export function generateBrowserTokens() {
         },
         'browser-components': {
           buildPath: BUILD_PATH,
+          prefix: PREFIX,
           files: componentFiles,
         },
         'browser-index': {
           buildPath: BUILD_PATH,
+          prefix: PREFIX,
           files: [
             {
               destination: 'index.js',
