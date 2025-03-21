f20580b7ca5a57d5a8aa0d169050322e8ed88ba6
Jord McCord <jmccord@uw.co.uk>
2025-03-21 11:43:03 +0000

    chore: add welcome page to docs

--- apps/storybook-docs/.storybook/preview.ts
+++ apps/storybook-docs/.storybook/preview.ts
@@ -2,12 +2,19 @@ import type { Preview } from '@storybook/react';
 import '@utilitywarehouse/hearth-css-reset';
 import '@utilitywarehouse/hearth-fonts';
 import '../../../shared/storybook/styles/preview.css';
+import '@utilitywarehouse/hearth-tokens/index.css';
+import '@utilitywarehouse/hearth-react/styles.css';
 import theme from '../../../shared/storybook/theme';
 
 const preview: Preview = {
   parameters: {
     docs: {
       theme,
+    },
+    options: {
+      storySort: {
+        order: ['Welcome', 'CSS Reset'],
+      },
     },
     controls: {
       matchers: {
