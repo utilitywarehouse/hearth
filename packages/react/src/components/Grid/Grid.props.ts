837578987fb408d1b92c75be088d0e5b8f22e320
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2025-03-21 13:19:46 +0000

    add text-align props to laout components (#179)

--- packages/react/src/components/Grid/Grid.props.ts
+++ packages/react/src/components/Grid/Grid.props.ts
@@ -10,6 +10,7 @@ import { PositionProps } from '../../props/position.props';
 import { PropDef } from '../../props/prop-def';
 import { SizeProps } from '../../props/size.props';
 import { SpacingProps } from '../../props/spacing.props';
+import { TextAlignProps } from '../../props/text-align.props';
 import { ComponentPropsWithout, RemovedProps } from '../../types/component-props';
 import { Responsive, Union } from '../../types/responsive';
 
@@ -56,7 +57,8 @@ interface CommonGridProps
     SizeProps,
     BorderProps,
     GridItemProps,
-    FlexItemProps {
+    FlexItemProps,
+    TextAlignProps {
   as?: 'div' | 'span';
   /**
    * Change the default rendered element for the one passed as a child, merging their props and behavior.
