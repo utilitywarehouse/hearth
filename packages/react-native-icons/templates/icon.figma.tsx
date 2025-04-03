164c2d4c389e7d9b70316df0fc13a12bbf999416
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2025-04-03 11:07:15 +0100

    fix RN figma template (#203)

--- packages/react-native-icons/templates/icon.figma.tsx
+++ packages/react-native-icons/templates/icon.figma.tsx
@@ -8,7 +8,7 @@ import figma from '@figma/code-connect';
 
 figma.connect(
   <%= name %>,
-  'https://www.figma.com/design/WDFaQF9EMtS7MjuIpjDVpf/UW-Icons?node-id=<%= id %>&m=dev',
+  'https://www.figma.com/design/x1DivEZ23UPZP7WXufHPjG/UW-Icons?node-id=<%= id %>&m=dev',
   {
     props: {},
     example: () => <<%= name %> />,
