9df2b855bc787e489e7aad406c5436dfeb669f11
Rob Phoenix <rphoenix@uw.co.uk>
2025-04-28 13:28:55 +0100

    update stories

--- apps/storybook-react/src/RadioGroup.stories.tsx
+++ apps/storybook-react/src/RadioGroup.stories.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { RadioGroup, Flex, Radio, RadioTile } from '@utilitywarehouse/hearth-react';
+import { RadioGroup, Flex, Radio, RadioTile, Box } from '@utilitywarehouse/hearth-react';
 
 const meta: Meta<typeof RadioGroup> = {
   title: 'Stories / RadioGroup',
@@ -33,44 +33,29 @@ type Story = StoryObj<typeof RadioGroup>;
 export const Playground: Story = {
   render: args => {
     return (
-      <Flex gap="400">
-        <RadioGroup {...args} name="with-radio">
-          <Radio value="1" label="One" />
-          <Radio value="2" label="Two" />
-          <Radio value="3" label="Three" />
-        </RadioGroup>
-        <RadioGroup {...args} name="with-radio-tile">
-          <RadioTile value="1" label="One" />
-          <RadioTile value="2" label="Two" />
-          <RadioTile value="3" label="Three" />
-        </RadioGroup>
-      </Flex>
+      <RadioGroup {...args} name="radio-group-example">
+        <RadioTile value="1" label="One" />
+        <RadioTile value="2" label="Two" />
+        <RadioTile value="3" label="Three" />
+      </RadioGroup>
     );
   },
 };
 
-export const RadioHelperAndValidationText: Story = {
-  name: 'Radio HelperText and ValidationText',
+export const RadioHelperText: Story = {
+  name: 'Radio HelperText',
   render: args => {
     return (
       <RadioGroup {...args}>
         <Radio value="1" label="One" helperText="One helper text" />
         <Radio value="2" label="Two" helperText="Two helper text" />
-        <Radio
-          value="3"
-          label="Three"
-          helperText="Three helper text"
-          invalid
-          validationText="Invalid"
-        />
+        <Radio value="3" label="Three" helperText="Three helper text" />
       </RadioGroup>
     );
   },
   args: {
     defaultValue: '3',
-    helperText: '',
-    validationText: 'Validation text',
-    validationStatus: undefined,
+    helperText: undefined,
   },
 };
 
@@ -106,7 +91,6 @@ export const Controlled: Story = {
     );
   },
   args: {
-    errorMessage: 'There is an error',
     label: 'What is your favourite animal?',
   },
 };
@@ -116,7 +100,12 @@ export const ShowingError: Story = {
   render: args => {
     const [selected, setSelected] = useState('');
     return (
-      <RadioGroup {...args} value={selected} onValueChange={setSelected} error={!selected}>
+      <RadioGroup
+        {...args}
+        value={selected}
+        onValueChange={setSelected}
+        validationStatus={selected ? undefined : 'invalid'}
+      >
         <Radio value="1" label="Bear" />
         <Radio value="2" label="Koala" />
         <Radio value="3" label="Wolf" />
@@ -125,24 +114,24 @@ export const ShowingError: Story = {
     );
   },
   args: {
-    errorMessage: 'Please tell us what your favourite animal is.',
+    validationText: 'Please tell us what your favourite animal is.',
     label: 'What is your favourite animal?',
     helperText: 'These are the best animals.',
   },
 };
 
-// export const Wrap: Story = {
-//   render: args => {
-//     return (
-//       <Box height="800px" width="400px" padding="16px">
-//         <RadioGroup {...args} direction="row" helperText="Child elements will wrap by default">
-//           <RadioTile value="1" label="One" />
-//           <RadioTile value="2" label="Two" />
-//           <RadioTile value="3" label="Three" />
-//           <RadioTile value="4" label="Four" />
-//           <RadioTile value="5" label="Five" />
-//         </RadioGroup>
-//       </Box>
-//     );
-//   },
-// };
+export const Wrap: Story = {
+  render: args => {
+    return (
+      <Box height="800px" width="400px" padding="200">
+        <RadioGroup {...args} direction="row" helperText="Child elements will wrap by default">
+          <RadioTile value="1" label="One" />
+          <RadioTile value="2" label="Two" />
+          <RadioTile value="3" label="Three" />
+          <RadioTile value="4" label="Four" />
+          <RadioTile value="5" label="Five" />
+        </RadioGroup>
+      </Box>
+    );
+  },
+};
