2721bb1a1f1e8d88df46fbb8f5917d53b2e50f0f
Jord McCord <jmccord@uw.co.uk>
2025-04-29 14:57:30 +0100

    fix: docs site links and cors

--- apps/storybook-docs/.storybook/main.ts
+++ apps/storybook-docs/.storybook/main.ts
@@ -2,6 +2,8 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 import { join, dirname } from 'path';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
@@ -24,22 +26,22 @@ const config: StorybookConfig = {
   refs: {
     react: {
       title: 'Hearth React',
-      url: 'https://uw-hearth-react.vercel.app',
+      url: isDev ? 'http://localhost:6003' : 'https://uw-hearth-react.vercel.app',
       expanded: false,
     },
     'react-native': {
       title: 'Hearth React Native',
-      url: 'https://uw-hearth-react-native.vercel.app',
+      url: isDev ? 'http://localhost:6004' : 'https://uw-hearth-react-native.vercel.app',
       expanded: false,
     },
     icons: {
       title: 'Hearth Icons',
-      url: 'https://uw-hearth-icons.vercel.app',
+      url: isDev ? 'http://localhost:6002' : 'https://uw-hearth-icons.vercel.app',
       expanded: false,
     },
     fonts: {
       title: 'Hearth Fonts',
-      url: 'https://uw-hearth-fonts.vercel.app',
+      url: isDev ? 'http://localhost:6001' : 'https://uw-hearth-fonts.vercel.app',
       expanded: false,
     },
   },
