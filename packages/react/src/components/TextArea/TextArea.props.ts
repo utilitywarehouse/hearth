b6866a5837a4c488e991452c461d5dd530942725
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2025-05-08 14:00:02 +0100

    Add `TextArea` component (#269)

---
+++ packages/react/src/components/TextArea/TextArea.props.ts
@@ -0,0 +1,35 @@
+import { ComponentPropsWithout, RemovedProps } from '../../types/component-props';
+
+export interface TextAreaProps extends ComponentPropsWithout<'textarea', RemovedProps | 'value'> {
+  /**
+   * The controlled value of the TextArea. Must be used with an `onChange` handler.
+   */
+  value?: string;
+
+  /**
+   * The label for the TextArea, describing its purpose.
+   */
+  label: string;
+
+  /**
+   * Optional helper text to provide additional context or instructions.
+   */
+  helperText?: string;
+
+  /**
+   * Indicates the validation state of the TextArea.
+   * @default undefined
+   */
+  validationStatus?: 'valid' | 'invalid';
+
+  /**
+   * Text to display when the `validationStatus` is set.
+   */
+  validationText?: string;
+
+  /**
+   * Controls the resize behavior of the TextArea.
+   * @default "both"
+   */
+  resize?: 'none' | 'vertical' | 'horizontal' | 'both';
+}
