250bf952e3e49aa024b99386bae08ea2697cc46f
Jord McCord <jmccord@uw.co.uk>
2025-05-13 12:31:24 +0100

    feat: `Accordion` active state

--- packages/react-native/src/components/Accordion/AccordionTrigger.tsx
+++ packages/react-native/src/components/Accordion/AccordionTrigger.tsx
@@ -5,20 +5,22 @@ import { useAccordionContext } from './Accordion.context';
 import { useAccordionItemContext } from './AccordionItem.context';
 import { StyleSheet } from 'react-native-unistyles';
 
-export const AccordionTrigger = forwardRef<PressableRef, PressableProps>(
-  ({ children, ...props }, ref) => {
-    const { noPadding: contextMoPadding } = useAccordionContext();
-    const { noPadding } = useAccordionItemContext();
-    const noPaddingValue = noPadding ?? contextMoPadding;
-    styles.useVariants({ noPadding: noPaddingValue });
+export const AccordionTrigger = forwardRef<
+  PressableRef,
+  PressableProps & { states?: { active?: boolean } }
+>(({ children, ...props }, ref) => {
+  const { active } = props.states || { active: false };
+  const { noPadding: contextMoPadding } = useAccordionContext();
+  const { noPadding } = useAccordionItemContext();
+  const noPaddingValue = noPadding ?? contextMoPadding;
+  styles.useVariants({ noPadding: noPaddingValue, active });
 
-    return (
-      <Pressable ref={ref} style={styles.trigger} {...props}>
-        {children}
-      </Pressable>
-    );
-  }
-);
+  return (
+    <Pressable ref={ref} style={styles.trigger} {...props}>
+      {children}
+    </Pressable>
+  );
+});
 
 AccordionTrigger.displayName = 'AccordionTrigger';
 
@@ -31,10 +33,24 @@ const styles = StyleSheet.create(theme => ({
     paddingVertical: theme.components.accordion.item.heading.paddingVertical,
     gap: theme.components.accordion.item.heading.gap,
     width: '100%',
+    _web: {
+      _hover: {
+        backgroundColor: theme.components.accordion.item.backgroundColorHover,
+      },
+      '_focus-visible': theme.helpers.focusVisible,
+      _active: {
+        backgroundColor: theme.components.accordion.item.backgroundColorActive,
+      },
+    },
     variants: {
       noPadding: {
         true: {
           paddingHorizontal: 0,
+        },
+      },
+      active: {
+        true: {
+          backgroundColor: theme.components.accordion.item.backgroundColorActive,
         },
       },
     },
