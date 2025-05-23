101acae8e46371f32b9a799dbe1e6c128b9e8d09
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2025-05-23 14:40:26 +0100

    Refactor `RadioCard` (#295)

--- packages/react/src/components/RadioCard/RadioCard.props.ts
+++ packages/react/src/components/RadioCard/RadioCard.props.ts
@@ -1,3 +1,4 @@
+import { MarginProps } from '../../props/margin.props';
 import { RadioProps } from '../Radio/Radio.props';
 
-export type RadioCardProps = Omit<RadioProps, 'helperText'>;
+export type RadioCardProps = Omit<RadioProps, 'helperText'> & MarginProps;
--- packages/react/src/components/RadioCard/RadioCard.tsx
+++ packages/react/src/components/RadioCard/RadioCard.tsx
@@ -3,31 +3,49 @@ import clsx from 'clsx';
 import type { ElementRef } from 'react';
 import type { RadioCardProps } from './RadioCard.props';
 import { withGlobalPrefix } from '../../helpers/with-global-prefix';
-import { SelectableCard } from '../SelectableCard/SelectableCard';
-import { Radio } from '../Radio/Radio';
-import { CardAction } from '../Card/CardAction';
+import {
+  Indicator as RadixRadioIndicator,
+  Item as RadixRadioItem,
+} from '@radix-ui/react-radio-group';
+import { useIds } from '../../hooks/use-ids';
+import { Label } from '../Label/Label';
+import { extractProps } from '../../helpers/extract-props';
+import { marginPropDefs } from '../../props/margin.props';
 
 const componentName = 'RadioCard';
 const componentClassName = withGlobalPrefix(componentName);
 
 type RadioCardElement = ElementRef<'button'>;
 
-export const RadioCard = React.forwardRef<RadioCardElement, RadioCardProps>(
-  ({ className, children, ...props }, ref) => {
-    return (
-      <SelectableCard
-        className={clsx(componentClassName, className)}
-        borderRadius="xl"
-        direction="column"
-        gap="150"
-      >
-        <CardAction>
-          <Radio ref={ref} {...props} labelFontWeight="semibold" />
-        </CardAction>
-        {children}
-      </SelectableCard>
-    );
-  }
-);
+export const RadioCard = React.forwardRef<RadioCardElement, RadioCardProps>((props, ref) => {
+  const {
+    className,
+    children,
+    id: providedId,
+    label,
+    'aria-labelledby': ariaLabelledby,
+    ...radioCardProps
+  } = extractProps(props, marginPropDefs);
+  const { id, labelId } = useIds({ providedId, prefix: 'radio' });
+  return (
+    <RadixRadioItem
+      ref={ref}
+      className={clsx(componentClassName, className)}
+      {...radioCardProps}
+      id={id}
+      aria-labelledby={ariaLabelledby ? ariaLabelledby : label ? labelId : undefined}
+    >
+      <div className="hearth-RadioContainer">
+        <div className="hearth-RadioItem">
+          <RadixRadioIndicator className="hearth-RadioIndicator" />
+        </div>
+        <Label id={labelId} htmlFor={id} disableUserSelect fontWeight="semibold">
+          {label}
+        </Label>
+      </div>
+      {children}
+    </RadixRadioItem>
+  );
+});
 
 RadioCard.displayName = componentName;
