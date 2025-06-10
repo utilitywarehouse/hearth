d911009059e64e42546e8b9c7c3d1001f682980e
Jord McCord <jmccord@uw.co.uk>
2025-06-10 11:30:22 +0100

    chore: align with native-ui updates (#343)

--- packages/react-native/docs/components/UsageWrap.tsx
+++ packages/react-native/docs/components/UsageWrap.tsx
@@ -1,22 +1,20 @@
 import React, { FC, PropsWithChildren } from 'react';
-import { Box, HearthUIProvider } from '../../src';
+import { Box } from '../../src';
 
 const UsageWrap: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className="sb-unstyled">
-      <HearthUIProvider>
-        <Box
-          mt="300"
-          p="200"
-          bg="white"
-          borderRadius="md"
-          borderWidth="1"
-          borderColor="grey1000"
-          width="100%"
-        >
-          {children}
-        </Box>
-      </HearthUIProvider>
+      <Box
+        mt="300"
+        p="200"
+        bg="white"
+        borderRadius="md"
+        borderWidth="1"
+        borderColor="grey1000"
+        width="100%"
+      >
+        {children}
+      </Box>
     </div>
   );
 };
