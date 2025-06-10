598ddfed6f21f8a37ed592729a5414c3101bf7bc
Jord McCord <jmccord@uw.co.uk>
2025-06-10 15:26:11 +0100

    refactor: update yellow inverted buttons (#345)

--- packages/react-native/src/hooks/useColorMode.ts
+++ packages/react-native/src/hooks/useColorMode.ts
@@ -1,8 +1,21 @@
-import useTheme from './useTheme';
+import { useEffect, useState } from 'react';
+import { UnistylesRuntime, useUnistyles } from 'react-native-unistyles';
 
 const useColorMode = () => {
-  const { colorMode } = useTheme();
-  return colorMode;
+  const {
+    theme: { colorMode: themeColorMode },
+  } = useUnistyles();
+
+  const [colorMode, setColorMode] = useState<'light' | 'dark'>(
+    UnistylesRuntime.themeName as 'light' | 'dark'
+  );
+
+  useEffect(() => {
+    if (colorMode === themeColorMode) return;
+    setColorMode(themeColorMode);
+  }, [themeColorMode, colorMode]);
+
+  return [colorMode, setColorMode] as const;
 };
 
 export default useColorMode;
