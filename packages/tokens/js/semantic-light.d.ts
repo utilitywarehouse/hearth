48fa440f47f69d401028dd9a174662ea30bdfcda
Jord McCord <jmccord@uw.co.uk>
2025-06-24 12:57:08 +0100

    feat: handle semantic tokens (#365)

---
+++ packages/tokens/js/semantic-light.d.ts
@@ -0,0 +1,25 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+declare const _default: {
+  readonly color: {
+    readonly background: '#fcfbf2';
+    readonly background1: '#ffffff';
+    readonly backgroundPurple: '#7a42c8';
+    readonly invalidText: '#de2612';
+  };
+  readonly opacity: {
+    readonly disabled: 50;
+  };
+  readonly shadow: {
+    readonly broadbandGreenColor: '#8cba3b';
+    readonly cashbackLilacColor: '#be7aeb';
+    readonly defaultColor: '#101010';
+    readonly energyBlueColor: '#6db5c3';
+    readonly insuranceOrangeColor: '#ff7b00';
+    readonly mobileRoseColor: '#ee6dbb';
+    readonly piggyPinkColor: '#f295ff';
+    readonly purpleColor: '#7a42c8';
+  };
+};
+export default _default;
