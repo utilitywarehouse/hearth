6e62678cb93eff740e0505894e21804fb6ee1a2f
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2025-09-16 14:41:08 +0100

    `Badge` component updates (#470)

--- packages/react/src/components/Badge/Badge.props.ts
+++ packages/react/src/components/Badge/Badge.props.ts
@@ -1,13 +1,17 @@
 import { PropDef } from '../../props/prop-def';
 import { TextTransformProps } from '../../props/text-transform.props';
 import { ComponentPropsWithout, RemovedProps } from '../../types/component-props';
+import { Responsive } from '../../types/responsive';
 
-const variants = ['solid', 'outline'] as const;
+const variants = ['emphasis', 'subtle', 'outline'] as const;
+const sizes = ['sm', 'md'] as const;
 
 export const badgePropDefs = {
-  variant: { className: 'variant', tokens: variants, responsive: false, default: 'solid' },
+  variant: { className: 'variant', tokens: variants, responsive: false, default: 'subtle' },
+  size: { className: 'size', tokens: sizes, responsive: true, default: 'md' },
 } satisfies {
   variant: PropDef<(typeof variants)[number]>;
+  size: PropDef<(typeof sizes)[number]>;
 };
 
 export interface BadgeProps
@@ -15,14 +19,31 @@ export interface BadgeProps
     TextTransformProps {
   /**
    * Sets the badges's visual variant
-   * @default solid
+   * @default subtle
    */
   variant?: (typeof variants)[number];
+  /**
+   * Sets the badge size.
+   * @default md
+   */
+  size?: Responsive<(typeof sizes)[number]>;
   /**
    * Sets the colour scheme.
    * @default info
    */
-  colorScheme?: 'info' | 'positive' | 'danger' | 'warning' | 'functional';
+  colorScheme?:
+    | 'info'
+    | 'positive'
+    | 'danger'
+    | 'warning'
+    | 'functional'
+    | 'energy'
+    | 'mobile'
+    | 'broadband'
+    | 'insurance'
+    | 'cashback'
+    | 'pig'
+    | 'highlight';
   /**
    * Removes the bottom radius, set when the Badge sits directly above another container
    * @default false
