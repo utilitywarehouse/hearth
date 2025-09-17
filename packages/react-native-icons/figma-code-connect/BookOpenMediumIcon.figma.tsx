ed0d8bbd1bb87e0d62e478af4b3a5847df6e2069
robphoenix <robphoenix@users.noreply.github.com>
2025-09-17 09:16:34 +0000

    Generate updated icons packages

---
+++ packages/react-native-icons/figma-code-connect/BookOpenMediumIcon.figma.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BookOpenMediumIcon } from '../lib';
+import figma from '@figma/code-connect';
+
+/**
+ * -- This file was auto-generated --
+ */
+
+figma.connect(
+  BookOpenMediumIcon,
+  'https://www.figma.com/design/x1DivEZ23UPZP7WXufHPjG/UW-Icons?node-id=2365:13&m=dev',
+  {
+    props: {},
+    example: () => <BookOpenMediumIcon />,
+  }
+);
