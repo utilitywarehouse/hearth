fc99389b014a87995b40c88852861b4c533e9982
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2025-10-10 12:54:08 +0100

    Add `ToggleButtonCard` (#533)

---
+++ packages/react/src/components/ToggleButtonCard/ToggleButtonCard.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { ToggleGroup as RadixToggleGroup } from 'radix-ui';
+import clsx from 'clsx';
+import { withGlobalPrefix } from '../../helpers/with-global-prefix';
+import type { ElementRef } from 'react';
+import { ToggleButtonCardProps } from './ToggleButtonCard.props';
+import { Card } from '../Card/Card';
+import { TickSmallIcon } from '@utilitywarehouse/hearth-react-icons';
+
+const COMPONENT_NAME = 'ToggleButtonCard';
+const componentClassName = withGlobalPrefix(COMPONENT_NAME);
+
+type ToggleButtonCardElement = ElementRef<'button'>;
+
+export const ToggleButtonCard = React.forwardRef<ToggleButtonCardElement, ToggleButtonCardProps>(
+  (
+    {
+      className,
+      children,
+      label,
+      'aria-label': ariaLabel,
+      'aria-labelledby': ariaLabelledBy,
+      'aria-describedby': ariaDescribedBy,
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <Card direction="column" className={clsx(componentClassName, className)}>
+        {children}
+        <RadixToggleGroup.Item
+          ref={ref}
+          {...props}
+          className={`${componentClassName}Item`}
+          aria-label={ariaLabel}
+          aria-labelledby={ariaLabelledBy}
+          aria-describedby={ariaDescribedBy}
+        >
+          <TickSmallIcon />
+          {label}
+        </RadixToggleGroup.Item>
+      </Card>
+    );
+  }
+);
+
+ToggleButtonCard.displayName = COMPONENT_NAME;
---
+++ packages/react/src/components/ToggleGroup/ToggleGroup.props.ts
@@ -0,0 +1,15 @@
+import { ToggleGroup as RadixToggleGroup } from 'radix-ui';
+import { ComponentPropsWithout } from '../../types/component-props';
+import { FlexProps } from '../Flex/Flex.props';
+import { MarginProps } from '../../props/margin.props';
+
+export interface ToggleGroupProps
+  extends ComponentPropsWithout<
+      typeof RadixToggleGroup.Root,
+      'asChild' | 'rovingFocus' | 'loop' | 'dir' | 'orientation' | 'disabled' | keyof FlexProps
+    >,
+    Omit<FlexProps, 'direction'>,
+    MarginProps {
+  /** The direction of the toggle items, will also set the aria-orientation value. */
+  direction?: 'column' | 'row';
+}
