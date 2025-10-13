42239b6a1bcee21dfad0f68445a0669981c0598f
Jord McCord <jmccord@uw.co.uk>
2025-10-13 10:35:13 +0100

    Export Figma Variables - 4 Collections (#540)
    Co-authored-by: UW Design Systems Bot <design-systems@uw.co.uk>

--- packages/tokens/js/components/dark/toast.d.ts
+++ packages/tokens/js/components/dark/toast.d.ts
@@ -4,7 +4,10 @@
 declare const _default: {
   readonly backgroundColor: '#3f3f3f';
   readonly borderRadius: 8;
-  readonly gapHorizontal: 8;
+  readonly gapHorizontal: 16;
   readonly padding: 14;
+  readonly stack: {
+    readonly gapHorizontal: 8;
+  };
 };
 export default _default;
--- packages/tokens/js/components/light/toast.d.ts
+++ packages/tokens/js/components/light/toast.d.ts
@@ -4,7 +4,10 @@
 declare const _default: {
   readonly backgroundColor: '#101010';
   readonly borderRadius: 8;
-  readonly gapHorizontal: 8;
+  readonly gapHorizontal: 16;
   readonly padding: 14;
+  readonly stack: {
+    readonly gapHorizontal: 8;
+  };
 };
 export default _default;
