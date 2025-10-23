405877eec98db0d03c6fc2b0f0408007f28a6057
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2025-10-23 16:18:48 +0100

    Add `rowSpacing` & `columnSpacing` props (#580)

--- packages/react/src/props/spacing.props.ts
+++ packages/react/src/props/spacing.props.ts
@@ -1,11 +1,15 @@
 import { PropDef } from './prop-def';
 
-const spacingValues = ['xs', 'sm', 'md', 'lg', 'xl'] as const;
+const spacingValues = ['none', '2xs', 'xs', 'sm', 'md', 'lg', 'xl', '2xl'] as const;
 
 const spacingPropDefs = {
   spacing: { className: 'spacing', tokens: spacingValues, responsive: false },
+  rowSpacing: { className: 'row-spacing', tokens: spacingValues, responsive: false },
+  columnSpacing: { className: 'column-spacing', tokens: spacingValues, responsive: false },
 } satisfies {
   spacing: PropDef<(typeof spacingValues)[number]>;
+  rowSpacing: PropDef<(typeof spacingValues)[number]>;
+  columnSpacing: PropDef<(typeof spacingValues)[number]>;
 };
 
 interface SpacingProps {
@@ -13,6 +17,14 @@ interface SpacingProps {
    * Set responsive spacing between child elements.
    */
   spacing?: (typeof spacingValues)[number];
+  /**
+   * Set responsive row spacing between child elements.
+   */
+  rowSpacing?: (typeof spacingValues)[number];
+  /**
+   * Set responsive column spacing between child elements.
+   */
+  columnSpacing?: (typeof spacingValues)[number];
 }
 
 export type { SpacingProps };
