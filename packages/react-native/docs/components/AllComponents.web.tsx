9fa6499b6eb1534c2ce7dc4352b1f848b94786ec
Michal Ciesliczka <michal.ciesliczka@gmail.com>
2025-10-28 15:43:32 +0100

    feat[RN]: add `IndicatorIconButton` component (#572)

--- packages/react-native/docs/components/AllComponents.web.tsx
+++ packages/react-native/docs/components/AllComponents.web.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 import { BottomSheetModalMethods } from '@gorhom/bottom-sheet/lib/typescript/types';
 import {
+  BellMediumIcon,
   BroadbandMediumIcon,
   CashbackCardMediumIcon,
   ChevronRightMediumIcon,
@@ -43,6 +44,7 @@ import {
   Icon,
   IconButton,
   IconContainer,
+  IndicatorIconButton,
   InlineLink,
   Input,
   LI,
@@ -383,6 +385,14 @@ const AllComponents: React.FC = () => {
           <ComponentWrapper name="Heading" link="/?path=/docs/typography-heading--docs">
             <Center flex={1}>
               <Heading>This is a Heading</Heading>
+            </Center>
+          </ComponentWrapper>
+          <ComponentWrapper
+            name="Indicator Icon Button"
+            link="/?path=/docs/components-indicator-icon-button--docs"
+          >
+            <Center flex={1}>
+              <IndicatorIconButton icon={BellMediumIcon} onPress={() => null} indicator={true} />
             </Center>
           </ComponentWrapper>
           <ComponentWrapper name="Icon Button" link="/?path=/docs/components-icon-button--docs">
---
+++ packages/react-native/src/components/IndicatorIconButton/IndicatorIconButton.stories.tsx
@@ -0,0 +1,142 @@
+import { Meta, StoryObj } from '@storybook/react-vite';
+import * as Icons from '@utilitywarehouse/hearth-react-native-icons';
+import { ComponentType } from 'react';
+import { VariantTitle } from '../../../docs/components';
+import { Box } from '../Box';
+import { Flex } from '../Flex';
+import { useTheme } from '../../hooks';
+import IndicatorIconButton from './IndicatorIconButton';
+
+const meta = {
+  title: 'Stories / IndicatorIconButton',
+  component: IndicatorIconButton,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    inverted: {
+      type: 'boolean',
+      control: 'boolean',
+      description: 'To set the button to be inverted. (To only be used on `purple` backgrounds)',
+    },
+    icon: {
+      options: ['none', ...Object.keys(Icons).filter(icon => icon.includes('Medium'))],
+      control: 'select',
+      description: 'The icon component for the button.',
+    },
+    pressed: {
+      type: 'boolean',
+      control: 'boolean',
+      description: 'To set the button to be pressed.',
+    },
+    indicator: {
+      type: 'boolean',
+      control: 'boolean',
+      description: 'To show or hide the red dot indicator.',
+    },
+  },
+  args: {
+    icon: 'BellMediumIcon' as unknown as ComponentType,
+    inverted: false,
+    pressed: false,
+    indicator: true,
+  },
+} satisfies Meta<typeof IndicatorIconButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Playground: Story = {
+  render: args => {
+    const icon = typeof args.icon === 'string' ? Icons[args.icon as keyof typeof Icons] : args.icon;
+    return <IndicatorIconButton {...args} icon={icon} />;
+  },
+};
+
+export const Variants: Story = {
+  render: args => {
+    const icon = typeof args.icon === 'string' ? Icons[args.icon as keyof typeof Icons] : args.icon;
+    return (
+      <Flex direction="column" space="xl">
+        <Flex direction="row" space="lg">
+          <Box>
+            <VariantTitle title="Without Indicator" invert={args.inverted}>
+              <IndicatorIconButton {...args} icon={icon} indicator={false} />
+            </VariantTitle>
+          </Box>
+          <Box>
+            <VariantTitle title="With Indicator" invert={args.inverted}>
+              <IndicatorIconButton {...args} icon={icon} indicator={true} />
+            </VariantTitle>
+          </Box>
+        </Flex>
+        <Flex direction="row" space="lg">
+          <Box style={{ backgroundColor: 'purple', padding: 10, borderRadius: 8 }}>
+            <VariantTitle title="Inverted" invert={!args.inverted}>
+              <IndicatorIconButton {...args} icon={icon} inverted={true} />
+            </VariantTitle>
+          </Box>
+        </Flex>
+      </Flex>
+    );
+  },
+};
+
+export const Colourful: Story = {
+  render: args => {
+    const icon = typeof args.icon === 'string' ? Icons[args.icon as keyof typeof Icons] : args.icon;
+    const theme = useTheme();
+
+    return (
+      <Flex direction="column" space="xl">
+        <Flex direction="row" space="lg">
+          <Box>
+            <IndicatorIconButton {...args} icon={icon} iconStyle={{ color: theme.color.energyBlue[500] }} />
+          </Box>
+          <Box>
+            <IndicatorIconButton {...args} icon={icon} iconStyle={{ color: theme.color.cashbackLilac[500] }} />
+          </Box>
+        </Flex>
+      </Flex>
+    );
+  },
+};
+
+export const WithAccessibilityLabel: Story = {
+  render: args => {
+    return (
+      <Flex direction="column" space="xl">
+        <Box>
+          <VariantTitle title="Notification label" invert={args.inverted}>
+            <IndicatorIconButton
+              {...args}
+              icon={Icons.BellMediumIcon}
+              indicator={true}
+              accessibilityLabel="New notifications available"
+            />
+          </VariantTitle>
+        </Box>
+        <Box>
+          <VariantTitle title="No new actions, cashback section" invert={args.inverted}>
+            <IndicatorIconButton
+              {...args}
+              icon={Icons.CashbackCardMediumIcon}
+              indicator={false}
+              accessibilityLabel="Cashback section"
+            />
+          </VariantTitle>
+        </Box>
+        <Box>
+          <VariantTitle title="New action, cashback section" invert={args.inverted}>
+            <IndicatorIconButton
+              {...args}
+              icon={Icons.CashbackCardMediumIcon}
+              indicator={true}
+              accessibilityLabel="Cashback section, new transactions"
+            />
+          </VariantTitle>
+        </Box>
+      </Flex>
+    );
+  },
+};
