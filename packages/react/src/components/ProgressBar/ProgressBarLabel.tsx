47b8b18f472ae584413a3ad8534be9d2b31aed30
Rob Phoenix <rphoenix@uw.co.uk>
2025-11-11 16:10:21 +0000

    small fixes

--- packages/react/src/components/ProgressBar/ProgressBarLabel.tsx
+++ packages/react/src/components/ProgressBar/ProgressBarLabel.tsx
@@ -1,22 +1,13 @@
 import * as React from 'react';
 import { BodyText } from '../BodyText/BodyText';
-import clsx from 'clsx';
 import { withGlobalPrefix } from '../../helpers/with-global-prefix';
 import { BodyTextProps } from '../BodyText/BodyText.props';
 
 const COMPONENT_NAME = 'ProgressBarLabel';
 const componentClassName = withGlobalPrefix(COMPONENT_NAME);
 
-export const ProgressBarLabel: React.FC<Pick<BodyTextProps, 'children' | 'className' | 'id'>> = ({
-  className,
-  ...props
-}) => (
-  <BodyText
-    as="span"
-    weight="semibold"
-    className={clsx(componentClassName, className)}
-    {...props}
-  />
+export const ProgressBarLabel: React.FC<Pick<BodyTextProps, 'children' | 'id'>> = props => (
+  <BodyText as="span" weight="semibold" className={componentClassName} {...props} />
 );
 
 ProgressBarLabel.displayName = COMPONENT_NAME;
--- packages/react/src/components/ProgressBar/ProgressBarValueText.tsx
+++ packages/react/src/components/ProgressBar/ProgressBarValueText.tsx
@@ -1,22 +1,13 @@
 import * as React from 'react';
 import { BodyText } from '../BodyText/BodyText';
-import clsx from 'clsx';
 import { withGlobalPrefix } from '../../helpers/with-global-prefix';
 import { BodyTextProps } from '../BodyText/BodyText.props';
 
 const COMPONENT_NAME = 'ProgressBarValueText';
 const componentClassName = withGlobalPrefix(COMPONENT_NAME);
 
-export const ProgressBarValueText: React.FC<Pick<BodyTextProps, 'children' | 'className'>> = ({
-  className,
-  ...props
-}) => (
-  <BodyText
-    as="span"
-    className={clsx(componentClassName, className)}
-    aria-hidden="true"
-    {...props}
-  />
+export const ProgressBarValueText: React.FC<Pick<BodyTextProps, 'children'>> = props => (
+  <BodyText as="span" className={componentClassName} aria-hidden="true" {...props} />
 );
 
 ProgressBarValueText.displayName = COMPONENT_NAME;
