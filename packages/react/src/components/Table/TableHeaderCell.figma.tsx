1adde016c7073b110c738c998a9ca1a8266ce836
Jord McCord <jmccord@uw.co.uk>
2025-12-08 15:14:36 +0000

    fix: add eslint ignore

--- packages/react/src/components/Table/TableHeaderCell.figma.tsx
+++ packages/react/src/components/Table/TableHeaderCell.figma.tsx
@@ -24,7 +24,7 @@ figma.connect(
       // "color": figma.enum('Color', {
       //   "Purple": "purple",
       //   "White": "white"
-      
+
       // })
     },
     example: (props) => <TableHeaderCell />,
