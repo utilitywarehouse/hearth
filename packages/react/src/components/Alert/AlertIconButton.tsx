6813f7e91df2e395a607177349fbf8ac0f90695e
Rob Phoenix <rphoenix@uw.co.uk>
2025-12-10 16:05:35 +0000

    update `Alert`

--- packages/react/src/components/Alert/AlertIconButton.tsx
+++ packages/react/src/components/Alert/AlertIconButton.tsx
@@ -2,8 +2,6 @@
 
 import { ChevronRightSmallIcon } from '@utilitywarehouse/hearth-react-icons';
 import clsx from 'clsx';
-import type { ElementRef } from 'react';
-import * as React from 'react';
 import { withGlobalPrefix } from '../../helpers/with-global-prefix';
 import { UnstyledIconButton } from '../UnstyledIconButton/UnstyledIconButton';
 import type { UnstyledIconButtonProps } from '../UnstyledIconButton/UnstyledIconButton.props';
@@ -11,24 +9,23 @@ import type { UnstyledIconButtonProps } from '../UnstyledIconButton/UnstyledIcon
 const COMPONENT_NAME = 'AlertIconButton';
 const componentClassName = withGlobalPrefix(COMPONENT_NAME);
 
-export type AlertIconButtonProps = UnstyledIconButtonProps;
-
-type AlertIconButtonElement = ElementRef<'button'>;
-
-export const AlertIconButton = React.forwardRef<AlertIconButtonElement, AlertIconButtonProps>(
-  ({ children, label = 'Alert action', title = 'Alert action', className, ...props }, ref) => {
-    return (
-      <UnstyledIconButton
-        ref={ref}
-        className={clsx(componentClassName, className)}
-        label={label}
-        title={title}
-        {...props}
-      >
-        {children || <ChevronRightSmallIcon />}
-      </UnstyledIconButton>
-    );
-  }
-);
+export const AlertIconButton = ({
+  children,
+  label = 'Alert action',
+  title = 'Alert action',
+  className,
+  ...props
+}: UnstyledIconButtonProps) => {
+  return (
+    <UnstyledIconButton
+      className={clsx(componentClassName, className)}
+      label={label}
+      title={title}
+      {...props}
+    >
+      {children || <ChevronRightSmallIcon />}
+    </UnstyledIconButton>
+  );
+};
 
 AlertIconButton.displayName = COMPONENT_NAME;
