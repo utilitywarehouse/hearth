6209228b62d43758662dc403574095a72f26f961
Rob Phoenix <rphoenix@uw.co.uk>
2025-12-19 16:59:39 +0000

    fix

--- apps/storybook-react/src/components/Box.stories.tsx
+++ apps/storybook-react/src/components/Box.stories.tsx
@@ -84,7 +84,7 @@ export default meta;
 type Story = StoryObj<typeof Box>;
 
 export const Playground: Story = {
-  render: args => <Box {...args} overflowX="scroll" />,
+  render: args => <Box {...args} />,
   args: {
     height: '128px',
     width: '128px',
