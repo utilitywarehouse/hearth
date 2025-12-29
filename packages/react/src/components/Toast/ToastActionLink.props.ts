ed7c608b523d5c9912750f306cc1299439448a12
Rob Phoenix <rphoenix@uw.co.uk>
2025-12-29 14:09:09 +0000

    fix props

--- packages/react/src/components/Toast/ToastActionLink.props.ts
+++ packages/react/src/components/Toast/ToastActionLink.props.ts
@@ -1,8 +1,7 @@
 import { Toast as ToastPrimitive } from 'radix-ui';
-import { InlineLink } from '../InlineLink/InlineLink';
 
 export type ToastActionLinkProps = Omit<
   React.ComponentPropsWithoutRef<typeof ToastPrimitive.Action>,
   'asChild'
 > &
-  React.ComponentPropsWithRef<typeof InlineLink>;
+  React.ComponentPropsWithRef<'a'>;
