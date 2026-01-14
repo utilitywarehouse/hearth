3e8495604225a6757ecdba1ff5ac359f994973bb
robphoenix <robphoenix@users.noreply.github.com>
2026-01-14 15:38:59 +0000

    Generate updated assets packages

---
+++ apps/storybook-assets/stories/IndividualAssets/SpotDarkModeLight.stories.tsx
@@ -0,0 +1,17 @@
+/* Don't edit this file directly, it was auto generated */
+import type { Meta, StoryObj } from '@storybook/react';
+import SpotDarkModeLightSrc from '@utilitywarehouse/hearth-svg-assets/lib/spot-dark-mode-light.svg';
+
+const meta: Meta = {
+  title: 'Individual Assets/SpotDarkModeLight',
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const SpotDarkModeLightStory: Story = {
+  name: 'SpotDarkModeLight',
+  render: () => (
+    <img src={SpotDarkModeLightSrc as unknown as string} alt="SpotDarkModeLight" style={{ maxWidth: 320 }} />
+  ),
+};
---
+++ apps/storybook-assets/stories/IndividualAssets/SpotNoBroadbandLight.stories.tsx
@@ -0,0 +1,17 @@
+/* Don't edit this file directly, it was auto generated */
+import type { Meta, StoryObj } from '@storybook/react';
+import SpotNoBroadbandLightSrc from '@utilitywarehouse/hearth-svg-assets/lib/spot-no-broadband-light.svg';
+
+const meta: Meta = {
+  title: 'Individual Assets/SpotNoBroadbandLight',
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const SpotNoBroadbandLightStory: Story = {
+  name: 'SpotNoBroadbandLight',
+  render: () => (
+    <img src={SpotNoBroadbandLightSrc as unknown as string} alt="SpotNoBroadbandLight" style={{ maxWidth: 320 }} />
+  ),
+};
