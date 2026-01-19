d250f7d8642d7ca7e073a33bbaea13154d8df5e3
Rob Phoenix <rphoenix@uw.co.uk>
2026-01-19 14:13:30 +0000

    `SectionHeader` code connect

---
+++ packages/react/src/figma/SectionHeader.figma.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { SectionHeader } from '../components/SectionHeader/SectionHeader';
+import figma from '@figma/code-connect';
+
+figma.connect(
+  SectionHeader,
+  'https://www.figma.com/design/6NKZXZhFSExXrcbBgc6zTR/Hearth-Components---Tokens?node-id=9092-3352&m=dev',
+  {
+    props: {
+      heading: figma.string('Heading'),
+      helperText: figma.boolean('Helper text?', {
+        true: figma.string('Helper text'),
+        false: undefined,
+      }),
+      badge: figma.enum('Badge?', {
+        true: figma.instance('Badge'),
+        false: undefined,
+      }),
+      // this is not fully working as expected due to figma limitations
+      trailingContent: figma.enum('Trailing content?', {
+        true: figma.children('Link'),
+      }),
+    },
+    example: ({ ...props }) => <SectionHeader {...props} />,
+  }
+);
