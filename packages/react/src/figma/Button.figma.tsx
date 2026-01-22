983245ee37705d6583ba81055203e029c7c31f3a
Rob Phoenix <rphoenix@uw.co.uk>
2026-01-22 08:59:27 +0000

    `Button` code connect

--- packages/react/src/figma/Button.figma.tsx
+++ packages/react/src/figma/Button.figma.tsx
@@ -2,39 +2,41 @@ import React from 'react';
 import { Button } from '../components/Button/Button';
 import figma from '@figma/code-connect';
 
-const sharedProps = {
-  iconLeft: figma.enum('Show icon left?', {
-    true: figma.instance('Icon left-20'),
-    false: undefined,
-  }),
-  iconRight: figma.enum('Show icon right?', {
-    true: figma.instance('Icon right-20'),
-    false: undefined,
-  }),
-  size: figma.enum('Size', {
-    'MD-48': 'md',
-    'SM-32': 'sm',
-  }),
-  inverted: figma.boolean('Inverted?'),
-  paddingNone: figma.boolean('Padding None?'),
-};
-
 figma.connect(
   Button,
   'https://www.figma.com/design/6NKZXZhFSExXrcbBgc6zTR/Hearth-Components---Tokens?node-id=90-432&m=dev',
   {
     props: {
-      ...sharedProps,
+      iconLeft: figma.enum('Show icon left?', {
+        true: figma.instance('Icon left-20'),
+        false: undefined,
+      }),
+      iconRight: figma.enum('Show icon right?', {
+        true: figma.instance('Icon right-20'),
+        false: undefined,
+      }),
+      size: figma.enum('Size', {
+        'MD-48': 'md',
+        'SM-32': 'sm',
+      }),
       variant: figma.enum('Variant', {
         Emphasis: 'emphasis',
+        Solid: 'solid',
+        Outline: 'outline',
+        Ghost: 'ghost',
       }),
       colorScheme: figma.enum('Color Scheme', {
         Highlight: 'highlight',
+        Functional: 'functional',
+        Affirmative: 'affirmative',
+        Destructive: 'destructive',
       }),
       children: figma.string('Text'),
+      inverted: figma.boolean('Inverted?'),
+      paddingNone: figma.boolean('Padding None?'),
     },
     example: ({ children, iconLeft, iconRight, ...props }) => (
-      <Button onClick={() => {}} {...props}>
+      <Button {...props}>
         {iconLeft}
         {children}
         {iconRight}
