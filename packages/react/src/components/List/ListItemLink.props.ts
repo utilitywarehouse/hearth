cc14cc4321c1365b833931dc34a3e171ff2ae6d5
Rob Phoenix <rphoenix@uw.co.uk>
2026-01-29 14:43:52 +0000

    add `asChild` prop to `ListItemLink`

--- packages/react/src/components/List/ListItemLink.props.ts
+++ packages/react/src/components/List/ListItemLink.props.ts
@@ -3,4 +3,9 @@ import { ListItemContentProps } from './ListItemContent.props';
 type ElementProps = React.ComponentPropsWithRef<'a'>;
 
 export interface ListItemLinkProps
-  extends ElementProps, Omit<ListItemContentProps, keyof ElementProps> {}
+  extends ElementProps, Omit<ListItemContentProps, keyof ElementProps> {
+  /**
+   * Change the default rendered element for the one passed as a child, merging their props and behavior.
+   */
+  asChild?: boolean;
+}
