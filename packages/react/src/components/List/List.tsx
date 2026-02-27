eed560d5f468928d5269db0d53f1f04445446017
Rob Phoenix <rphoenix@uw.co.uk>
2026-02-27 12:12:38 +0000

    🐛 [FIX]: Improve labelling of `List`

--- packages/react/src/components/List/List.tsx
+++ packages/react/src/components/List/List.tsx
@@ -6,12 +6,14 @@ import { Box } from '../Box/Box';
 import { extractProps } from '../../helpers/extract-props';
 import { marginPropDefs } from '../../props/margin.props';
 import { SectionHeader } from '../SectionHeader/SectionHeader';
+import { useIds } from '../../hooks/use-ids';
 
 const COMPONENT_NAME = 'List';
 const componentClassName = withGlobalPrefix(COMPONENT_NAME);
 
 export const List = (props: ListProps) => {
   const {
+    id: providedId,
     as: Tag = 'ul',
     className,
     colorScheme = undefined,
@@ -24,6 +26,8 @@ export const List = (props: ListProps) => {
     validationStatus,
     children,
     paddingNone,
+    'aria-label': ariaLabel,
+    'aria-labelledby': ariaLabelledby,
     ...listProps
   } = extractProps(props, marginPropDefs);
 
@@ -36,20 +40,19 @@ export const List = (props: ListProps) => {
     validationStatus,
   };
 
-  const dataAttributeProps = {
+  const { id } = useIds({ providedId });
+
+  const attributeProps = {
+    'aria-label': ariaLabel,
+    'aria-labelledby': ariaLabelledby || id,
     'data-padding-none': paddingNone ? '' : undefined,
   };
 
   return (
     <div className={cn(componentClassName, className)}>
-      {heading ? <SectionHeader {...sectionHeaderProps} /> : null}
+      {heading ? <SectionHeader id={id} {...sectionHeaderProps} /> : null}
       {variant === undefined || colorScheme === undefined ? (
-        <Box
-          asChild
-          className={`${componentClassName}Container`}
-          role="list"
-          {...dataAttributeProps}
-        >
+        <Box asChild className={`${componentClassName}Container`} role="list" {...attributeProps}>
           <Tag {...listProps}>{children}</Tag>
         </Box>
       ) : (
@@ -59,7 +62,7 @@ export const List = (props: ListProps) => {
           variant={variant}
           colorScheme={colorScheme}
         >
-          <Tag role="list" {...listProps} {...dataAttributeProps}>
+          <Tag role="list" {...listProps} {...attributeProps}>
             {children}
           </Tag>
         </Card>
