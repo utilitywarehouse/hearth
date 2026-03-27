e61fa8f6f90603bae796f9e86a20fcb3febdc4a7
Rob Phoenix <rphoenix@uw.co.uk>
2026-03-27 14:32:16 +0000

    update props

--- packages/react/src/components/Modal/Modal.props.ts
+++ packages/react/src/components/Modal/Modal.props.ts
@@ -1,22 +1,32 @@
 import { Dialog as DialogPrimitive } from 'radix-ui';
 import type { ComponentPropsWithRef, ComponentPropsWithoutRef, ReactNode } from 'react';
 
-export interface ModalProps
+interface BaseModalProps
   extends
     ComponentPropsWithoutRef<typeof DialogPrimitive.DialogPortal>,
     Omit<ComponentPropsWithoutRef<typeof DialogPrimitive.Content>, 'asChild' | 'forceMount'>,
     ComponentPropsWithRef<'div'> {
-  heading: string;
   description?: string;
   hideCloseButton?: boolean;
   fullScreen?: boolean;
   image?: ReactNode;
-  loading?: boolean;
   // @deprecated Please use loadingHeading and loadingDescription instead
   loadingText?: string;
   loadingHeading?: string;
   loadingDescription?: string;
 }
+
+type NonLoadingState = {
+  loading?: false;
+  heading: string; // Required
+};
+
+type LoadingState = {
+  loading: true;
+  heading?: string; // Heading becomes optional
+};
+
+export type ModalProps = BaseModalProps & (NonLoadingState | LoadingState);
 
 export type ModalCloseProps = Omit<
   ComponentPropsWithRef<typeof DialogPrimitive.DialogClose>,
