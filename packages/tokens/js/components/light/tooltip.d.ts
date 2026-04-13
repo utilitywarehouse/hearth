03b7532a018e9a672cc9d85726c496ca8b28e964
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2026-04-13 09:39:46 +0100

    Export Figma Variables - 5 Collections (#1112)
    Co-authored-by: UW Design Systems Bot <design-systems@uw.co.uk>

--- packages/tokens/js/components/light/tooltip.d.ts
+++ packages/tokens/js/components/light/tooltip.d.ts
@@ -6,6 +6,7 @@ declare const _default: {
   readonly borderRadius: 8;
   readonly gapHorizontal: 8;
   readonly gapVertical: 2;
+  readonly maxWidth: 680;
   readonly paddingHorizontal: 12;
   readonly paddingVertical: 8;
 };
