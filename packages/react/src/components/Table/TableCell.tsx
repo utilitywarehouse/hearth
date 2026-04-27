2b3811c93d57ed97ad49f38b1258293d6f03a959
Rob Phoenix <rphoenix@uw.co.uk>
2026-04-27 13:26:49 +0100

    💅 [ENHANCEMENT]: Add `textAlign` prop to `TableCell` & `TableHeaderCell` components

--- packages/react/src/components/Table/TableCell.tsx
+++ packages/react/src/components/Table/TableCell.tsx
@@ -4,20 +4,21 @@ import type { ComponentRef } from 'react';
 import { cn } from '../../helpers/cn';
 import { withGlobalPrefix } from '../../helpers/with-global-prefix';
 import type { TableCellProps } from './Table.props';
+import { extractProps } from '../../helpers/extract-props';
+import { textAlignPropDefs } from '../../props/text-align.props';
 
 const COMPONENT_NAME = 'TableCell';
 const componentClassName = withGlobalPrefix(COMPONENT_NAME);
 
 type TableCellElement = ComponentRef<'td'>;
 
-export const TableCell = forwardRef<TableCellElement, TableCellProps>(
-  ({ className, children, ...props }, ref) => {
-    return (
-      <td ref={ref} className={cn(componentClassName, className)} {...props}>
-        {children}
-      </td>
-    );
-  }
-);
+export const TableCell = forwardRef<TableCellElement, TableCellProps>((props, ref) => {
+  const { className, children, ...tdProps } = extractProps(props, textAlignPropDefs);
+  return (
+    <td ref={ref} className={cn(componentClassName, className)} {...tdProps}>
+      {children}
+    </td>
+  );
+});
 
 TableCell.displayName = COMPONENT_NAME;
