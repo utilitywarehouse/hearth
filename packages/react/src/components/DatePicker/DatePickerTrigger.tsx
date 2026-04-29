10f80e17a17b53c851195f2d1e9ba1e0509de2ce
Rob Phoenix <rphoenix@uw.co.uk>
2026-04-29 13:16:45 +0100

    checks

--- packages/react/src/components/DatePicker/DatePickerTrigger.tsx
+++ packages/react/src/components/DatePicker/DatePickerTrigger.tsx
@@ -10,29 +10,28 @@ import { CalendarSmallIcon } from '@utilitywarehouse/hearth-react-icons';
 const COMPONENT_NAME = 'DatePickerTrigger';
 const componentClassName = withGlobalPrefix(COMPONENT_NAME);
 
-export const DatePickerTrigger = forwardRef<
-  ComponentRef<'button'>,
-  DatePickerTriggerProps
->(({ className, value, placeholder = 'DD/MM/YYYY', disabled, onClick, ...props }, ref) => {
-  return (
-    <button
-      ref={ref}
-      className={cn(componentClassName, className)}
-      aria-disabled={disabled || undefined}
-      onClick={disabled ? undefined : onClick}
-      type="button"
-      {...props}
-    >
-      {value ? (
-        <div className={`${componentClassName}Value`}>{value}</div>
-      ) : placeholder ? (
-        <div className={`${componentClassName}Placeholder`}>{placeholder}</div>
-      ) : null}
-      <div className={`${componentClassName}Icon`}>
-        <CalendarSmallIcon />
-      </div>
-    </button>
-  );
-});
+export const DatePickerTrigger = forwardRef<ComponentRef<'button'>, DatePickerTriggerProps>(
+  ({ className, value, placeholder = 'DD/MM/YYYY', disabled, onClick, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        className={cn(componentClassName, className)}
+        aria-disabled={disabled || undefined}
+        onClick={disabled ? undefined : onClick}
+        type="button"
+        {...props}
+      >
+        {value ? (
+          <div className={`${componentClassName}Value`}>{value}</div>
+        ) : placeholder ? (
+          <div className={`${componentClassName}Placeholder`}>{placeholder}</div>
+        ) : null}
+        <div className={`${componentClassName}Icon`}>
+          <CalendarSmallIcon />
+        </div>
+      </button>
+    );
+  }
+);
 
 DatePickerTrigger.displayName = COMPONENT_NAME;
