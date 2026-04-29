bc13e50880eaaaddfeb8d44654cffb566927f832
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2026-04-29 13:21:03 +0100

    🐛 [FIX]: `DatePickerTrigger` ref not forwarded to underlying button element (#1174)

--- packages/react/src/components/DatePicker/DatePickerTrigger.tsx
+++ packages/react/src/components/DatePicker/DatePickerTrigger.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { forwardRef } from 'react';
+import type { ComponentRef } from 'react';
 import { withGlobalPrefix } from '../../helpers/with-global-prefix';
 import type { DatePickerTriggerProps } from './DatePickerTrigger.props';
 import { cn } from '../../helpers/cn';
@@ -8,32 +10,28 @@ import { CalendarSmallIcon } from '@utilitywarehouse/hearth-react-icons';
 const COMPONENT_NAME = 'DatePickerTrigger';
 const componentClassName = withGlobalPrefix(COMPONENT_NAME);
 
-export const DatePickerTrigger = ({
-  className,
-  value,
-  placeholder = 'DD/MM/YYYY',
-  disabled,
-  onClick,
-  ...props
-}: DatePickerTriggerProps) => {
-  return (
-    <button
-      className={cn(componentClassName, className)}
-      aria-disabled={disabled || undefined}
-      onClick={disabled ? undefined : onClick}
-      type="button"
-      {...props}
-    >
-      {value ? (
-        <div className={`${componentClassName}Value`}>{value}</div>
-      ) : placeholder ? (
-        <div className={`${componentClassName}Placeholder`}>{placeholder}</div>
-      ) : null}
-      <div className={`${componentClassName}Icon`}>
-        <CalendarSmallIcon />
-      </div>
-    </button>
-  );
-};
+export const DatePickerTrigger = forwardRef<ComponentRef<'button'>, DatePickerTriggerProps>(
+  ({ className, value, placeholder = 'DD/MM/YYYY', disabled, onClick, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        className={cn(componentClassName, className)}
+        aria-disabled={disabled || undefined}
+        onClick={disabled ? undefined : onClick}
+        type="button"
+        {...props}
+      >
+        {value ? (
+          <div className={`${componentClassName}Value`}>{value}</div>
+        ) : placeholder ? (
+          <div className={`${componentClassName}Placeholder`}>{placeholder}</div>
+        ) : null}
+        <div className={`${componentClassName}Icon`}>
+          <CalendarSmallIcon />
+        </div>
+      </button>
+    );
+  }
+);
 
 DatePickerTrigger.displayName = COMPONENT_NAME;
