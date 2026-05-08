fde2d29187139719e32f52b634bc41da593e24fb
Rob Phoenix <rphoenix@uw.co.uk>
2026-05-08 15:00:59 +0100

    update shared config

--- shared/storybook/theme.ts
+++ shared/storybook/theme.ts
@@ -1,14 +1,5 @@
-import { create } from 'storybook/theming';
 // @ts-expect-error - logo is not a module
 import logo from './assets/logo.svg';
-
-export default create({
-  base: 'light',
-  brandTitle: 'Hearth Design Systems',
-  brandUrl: 'https://hearth.prod.uw.systems/',
-  brandImage: logo,
-  brandTarget: '_self',
-});
 
 export const config = {
   base: 'light',
