f19b97c589cda1e985351f2ff6098afc62f65ca8
Rob Phoenix <rphoenix@uw.co.uk>
2026-05-13 15:34:20 +0100

    update Accordion props docs

--- packages/react/src/components/Accordion/AccordionHeader.props.ts
+++ packages/react/src/components/Accordion/AccordionHeader.props.ts
@@ -5,5 +5,8 @@ export interface AccordionHeaderProps extends Omit<
   ComponentPropsWithRef<typeof AccordionPrimitive.Header>,
   'asChild'
 > {
+  /*
+   * Render the appropriate heading level for your page
+   */
   as?: 'h1' | 'h2' | 'h3' | 'h4';
 }
