e933f60a2940b69e1fe8ff2eb93e1838f1adf343
Rob Phoenix <rphoenix@uw.co.uk>
2026-05-13 16:57:16 +0100

    add MCP playground

---
+++ apps/playground-mcp/.eslintrc.js
@@ -0,0 +1,5 @@
+/* eslint-env node */
+module.exports = {
+  extends: ['../../.eslintrc.js', 'next/core-web-vitals', 'next/typescript'],
+  ignorePatterns: ['.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+};
