dfaae7688567b2cddcd5f00f1053ac279d27d7e5
Vasile Schipor <vschipor@uw.co.uk>
2026-05-14 11:21:33 +0300

    docs(Rating): add emoji variant documentation and Figma connect

--- packages/react-native/src/components/Rating/Rating.figma.tsx
+++ packages/react-native/src/components/Rating/Rating.figma.tsx
@@ -18,3 +18,22 @@ figma.connect(
     example: props => <Rating value={props.value} />,
   }
 );
+
+figma.connect(
+  Rating,
+  'https://www.figma.com/design/6NKZXZhFSExXrcbBgc6zTR/Hearth-Components---Tokens?node-id=10620-4185',
+  {
+    variant: { Variant: 'Emojis' },
+    props: {
+      value: figma.enum('Rating', {
+        '0 Star': 0,
+        '1 Star': 1,
+        '2 Star': 2,
+        '3 Star': 3,
+        '4 Star': 4,
+        '5 Star': 5,
+      }),
+    },
+    example: props => <Rating value={props.value} variant="emojis" />,
+  }
+);
