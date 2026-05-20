6328039f45f3bbce45e01db685eb1cf9549473d3
Rob Phoenix <rphoenix@uw.co.uk>
2026-05-20 13:53:13 +0100

    Remove playground-nextjs

--- apps/playground-nextjs/postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: {
-    '@csstools/postcss-global-data': {
-      files: ['./node_modules/@utilitywarehouse/hearth-react/breakpoints.css'],
-    },
-    'postcss-custom-media': {},
-    autoprefixer: {},
-  },
-};
--- apps/playground-nextjs/src/components/StyledBox.tsx
+++
@@ -1,4 +0,0 @@
-import { Box as HearthBox } from '@utilitywarehouse/hearth-react';
-import { styled } from '@mui/material';
-
-export const StyledBox = styled(HearthBox)({});
