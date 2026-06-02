27dab2d78250737c6844e38c369368e60e360529
Rob Phoenix <rphoenix@uw.co.uk>
2026-06-02 14:17:57 +0100

    remove MCP playground

--- apps/playground-mcp/.eslintrc.js
+++
@@ -1,5 +0,0 @@
-/* eslint-env node */
-module.exports = {
-  extends: ['../../.eslintrc.js', 'next/core-web-vitals', 'next/typescript'],
-  ignorePatterns: ['.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
-};
