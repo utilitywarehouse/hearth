008abefe512c2ef5263c030383a3122aed22d386
Rob Phoenix <rphoenix@uw.co.uk>
2026-06-05 14:47:43 +0100

    fix tokens script

--- packages/tokens/scripts/helpers/px.js
+++ packages/tokens/scripts/helpers/px.js
@@ -1,1 +1,7 @@
-export const px = token => (typeof token === number ? token.value + 'px' : token.value);
+export const px = token => {
+  // filter out CSS custom properties
+  if (typeof token.value === 'string' && token.value.includes('var')) {
+    return token.value;
+  }
+  return token.value + 'px';
+};
