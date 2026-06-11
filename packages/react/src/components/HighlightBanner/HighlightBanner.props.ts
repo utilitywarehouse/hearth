f2ae0bd31692379dbd1a9d89a66a2c10959ed18b
Rob Phoenix <rphoenix@uw.co.uk>
2026-06-11 14:42:22 +0100

    💅 [ENHANCEMENT]: Add `shadowColor` prop to `HighlightBanner` component

--- packages/react/src/components/HighlightBanner/HighlightBanner.props.ts
+++ packages/react/src/components/HighlightBanner/HighlightBanner.props.ts
@@ -1,7 +1,9 @@
 import type { ComponentPropsWithRef } from 'react';
 import { MarginProps } from '../../props/margin.props';
+import { CardProps } from '../Card/Card.props';
 
-export interface HighlightBannerProps extends ComponentPropsWithRef<'div'>, MarginProps {
+export interface HighlightBannerProps
+  extends ComponentPropsWithRef<'div'>, Pick<CardProps, 'shadowColor'>, MarginProps {
   colorScheme?: 'neutralStrong' | 'neutralSubtle';
   heading: string;
   headingColor: 'pig' | 'energy' | 'broadband' | 'mobile' | 'insurance' | 'cashback' | 'highlight';
