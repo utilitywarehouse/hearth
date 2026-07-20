8ea436149c16199f7f72caa816696978820e590d
jordmccord <7591840+jordmccord@users.noreply.github.com>
2026-07-20 08:58:26 +0000

    Generate updated icons packages

---
+++ apps/storybook-docs/docs/icons/IndividualIcons/BudgetPlanMediumIcon.stories.ts
@@ -0,0 +1,19 @@
+/* Don't edit this file directly, it was auto generated */
+import type { Meta, StoryObj } from '@storybook/react';
+import { BudgetPlanMediumIcon } from '@utilitywarehouse/hearth-react-icons/lib/BudgetPlanMediumIcon';
+
+const meta: Meta<typeof BudgetPlanMediumIcon> = {
+  title: 'Icons / Individual Icons/BudgetPlanMediumIcon',
+  component: BudgetPlanMediumIcon,
+};
+
+export default meta;
+type Story = StoryObj<typeof BudgetPlanMediumIcon>;
+
+export const BudgetPlanMediumIconStory: Story = {
+  name: 'BudgetPlanMediumIcon',
+  argTypes: {
+    children: { table: { disable: true } },
+    color: { table: { disable: true } },
+  },
+};
