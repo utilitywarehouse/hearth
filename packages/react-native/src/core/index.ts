0278d00cb733aac3e08b0dea2967a0ec683805a8
Jord McCord <jmccord@uw.co.uk>
2026-08-05 17:57:52 +0100

    fix: exports

--- packages/react-native/src/core/index.ts
+++ packages/react-native/src/core/index.ts
@@ -1,5 +1,24 @@
 export * from './breakpoints';
 export * from './themes';
-export * from 'react-native-unistyles';
+export {
+  Display,
+  Hide,
+  ScopedTheme,
+  StyleSheet,
+  UnistylesRuntime,
+  createUnistylesElement,
+  getServerUnistyles,
+  hydrateServerUnistyles,
+  mq,
+  resetServerUnistyles,
+  useServerUnistyles,
+  useUnistyles,
+  withUnistyles,
+} from 'react-native-unistyles';
+export type {
+  UnistylesBreakpoints,
+  UnistylesThemes,
+  UnistylesVariants,
+} from 'react-native-unistyles';
 
 export type * from '../types/unistyles';
