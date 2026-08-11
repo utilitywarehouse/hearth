9676b44f51889405a15fa5c94a2174510b43bc0c
Rob Phoenix <9257284+robphoenix@users.noreply.github.com>
2026-08-11 10:05:45 +0100

    Export Figma Variables - 4 Collections (#1510)
    Co-authored-by: Hearth Design Systems Bot <design-systems@uw.co.uk>

--- packages/tokens/browser/color.d.ts
+++ packages/tokens/browser/color.d.ts
@@ -78,6 +78,8 @@ export declare const color: {
     readonly '900': 'var(--h-color-orange-900)';
   };
   readonly piggyPink: {
+    readonly '0': 'var(--h-color-piggy-pink-0)';
+    readonly '100': 'var(--h-color-piggy-pink-100)';
     readonly '200': 'var(--h-color-piggy-pink-200)';
     readonly '300': 'var(--h-color-piggy-pink-300)';
     readonly '800': 'var(--h-color-piggy-pink-800)';
