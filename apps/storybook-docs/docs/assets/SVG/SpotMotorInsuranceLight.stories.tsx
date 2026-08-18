0b2662e026e46e5027d754ecc7a79c3361aac376
robphoenix <9257284+robphoenix@users.noreply.github.com>
2026-08-18 11:28:32 +0000

    Generate updated SVG assets packages

---
+++ apps/storybook-docs/docs/assets/SVG/SpotMotorInsuranceLight.stories.tsx
@@ -0,0 +1,17 @@
+/* Don't edit this file directly, it was auto generated */
+import type { Meta, StoryObj } from '@storybook/react';
+import SpotMotorInsuranceLight from '@utilitywarehouse/hearth-svg-assets/lib/spot-motor-insurance-light.svg';
+
+const meta: Meta = {
+  title: 'Assets / SVG / SpotMotorInsuranceLight',
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const SpotMotorInsuranceLightStory: Story = {
+  name: 'SpotMotorInsuranceLight',
+  render: () => (
+    <img src={SpotMotorInsuranceLight as unknown as string} alt="SpotMotorInsuranceLight" style={{ maxWidth: 320 }} />
+  ),
+};
